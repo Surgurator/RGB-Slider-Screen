7481eb59d903e21fab3e657b8278c9e9deed99f1
Surgurator <a9191084222@gmail.com>
2022-11-18 15:34:35 +0300

    Constraints ver 0.1

--- RGB Slider Screen/ViewController.swift
+++ RGB Slider Screen/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var viewColor: UIView!
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
